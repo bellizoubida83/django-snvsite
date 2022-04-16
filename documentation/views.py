757413eb527129152a80a0c8535e6cdedfb9027c
Belli Zoubida <bellizoubida@Bellis-MacBook-Pro.local>
2022-04-17 00:03:47 +0100

    add some urls

--- documentation/views.py
+++ documentation/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
+
+from .models import Documentation
+
 # Create your views here.
 
 
 def doc_list(request):
-    pass
+    doc_list=Documentation.objects.all()
+    print(doc_list)
+    context ={'doc': doc_list}
+    return render(request,'documentation/doc_list.html', context)
 
 def doc_detail(request,id):
         pass
